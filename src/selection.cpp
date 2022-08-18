08e6c3ce33d4e8a61aac545ea55b6a6a8131779a
David Andrs <andrsd@gmail.com>
2022-08-18 14:36:52 -0600

    Selection mode: points

---
+++ src/selection.cpp
@@ -0,0 +1,84 @@
+#include "selection.h"
+#include "vtkActor.h"
+#include "vtkDataSetMapper.h"
+#include "vtkCompositeDataGeometryFilter.h"
+#include "vtkExtractSelection.h"
+#include "vtkUnstructuredGrid.h"
+#include "vtkSelection.h"
+#include "vtkSelectionNode.h"
+
+Selection::Selection(vtkAlgorithmOutput * input_data) :
+    geometry(vtkCompositeDataGeometryFilter::New()),
+    extract_selection(vtkExtractSelection::New()),
+    selected(vtkUnstructuredGrid::New()),
+    mapper(vtkDataSetMapper::New()),
+    actor(vtkActor::New()),
+    selection(vtkSelection::New())
+{
+    this->geometry->SetInputConnection(0, input_data);
+    this->geometry->Update();
+    this->extract_selection->SetInputData(0, this->geometry->GetOutput());
+    this->mapper->SetInputData(this->selected);
+    this->actor->SetMapper(this->mapper);
+}
+
+Selection::~Selection()
+{
+    this->geometry->Delete();
+    this->extract_selection->Delete();
+    this->selected->Delete();
+    this->mapper->Delete();
+    this->actor->Delete();
+    this->selection->Delete();
+}
+
+vtkActor *
+Selection::getActor() const
+{
+    return this->actor;
+}
+
+vtkUnstructuredGrid *
+Selection::getSelected() const
+{
+    return this->selected;
+}
+
+void
+Selection::clear()
+{
+    this->selected->Initialize();
+    this->mapper->SetInputData(this->selected);
+}
+
+void
+Selection::selectPoint(const vtkIdType & point_id)
+{
+    auto * ids = vtkIdTypeArray::New();
+    ids->SetNumberOfComponents(1);
+    ids->InsertNextValue(point_id);
+
+    auto * selection_node = vtkSelectionNode::New();
+    selection_node->SetFieldType(vtkSelectionNode::POINT);
+    selection_node->SetContentType(vtkSelectionNode::INDICES);
+    selection_node->SetSelectionList(ids);
+
+    setSelection(selection_node);
+}
+
+void
+Selection::setSelection(vtkSelectionNode * selection_node)
+{
+    this->selection->Delete();
+    this->selection = vtkSelection::New();
+    this->selection->AddNode(selection_node);
+
+    this->extract_selection->SetInputData(1, this->selection);
+    this->extract_selection->Update();
+
+    this->selected->Delete();
+    this->selected = vtkUnstructuredGrid::New();
+    this->selected->ShallowCopy(this->extract_selection->GetOutput());
+
+    this->mapper->SetInputData(this->selected);
+}
---
+++ src/selection.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "vtkType.h"
+
+class vtkActor;
+class vtkDataSetMapper;
+class vtkCompositeDataGeometryFilter;
+class vtkExtractSelection;
+class vtkUnstructuredGrid;
+class vtkSelection;
+class vtkSelectionNode;
+class vtkAlgorithmOutput;
+
+class Selection {
+public:
+    Selection(vtkAlgorithmOutput * input_data);
+    virtual ~Selection();
+
+    vtkActor * getActor() const;
+    vtkUnstructuredGrid * getSelected() const;
+    void clear();
+    void selectPoint(const vtkIdType & point_id);
+
+protected:
+    void setSelection(vtkSelectionNode * selection_node);
+
+    vtkCompositeDataGeometryFilter * geometry;
+    vtkExtractSelection * extract_selection;
+    vtkUnstructuredGrid * selected;
+    vtkDataSetMapper * mapper;
+    vtkActor * actor;
+    vtkSelection * selection;
+};
