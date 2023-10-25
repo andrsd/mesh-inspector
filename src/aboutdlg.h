d172df5aa7c9588e27e987e8832359141ed0b97e
David Andrs <andrsd@gmail.com>
2023-10-25 08:48:35 -0600

    Adding a clickable link to home page on the About dialog

--- src/aboutdlg.h
+++ src/aboutdlg.h
@@ -4,15 +4,21 @@
 
 class QVBoxLayout;
 class QLabel;
+class ClickableLabel;
 
 class AboutDialog : public QDialog {
 public:
     explicit AboutDialog(QWidget * parent = nullptr);
     ~AboutDialog() override;
 
+protected slots:
+    void onHomepageClicked();
+
 protected:
     QVBoxLayout * layout;
     QLabel * icon;
     QLabel * title;
-    QLabel * text;
+    QLabel * version;
+    ClickableLabel * homepage;
+    QLabel * copyright;
 };
